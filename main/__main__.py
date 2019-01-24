db7b8e8662d718f16d8ed9f57bd6223b25fe9a1c
wangchongyang <wangchongyang@siasun.com>
2019-01-24 10:52:51 +0800

    Initial commit

---
+++ main/__main__.py
@@ -0,0 +1,10 @@
+# Step 1: Importing the libraries
+
+
+import numpy as np
+import pandas as pd
+
+if __name__ is "__main__":
+    print("hello,machine-learning")
+    a = np.array([1, 2, 3])
+    print(a)
